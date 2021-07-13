bb31750ee49f92df3eb5205f3c780024b9db7ff6
ron93 <ronmkare1@gmail.com>
2021-07-13 17:05:23 +0300

    declaring service -- Dbcontext dependecy injection

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DocumentUpoadAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,8 @@ namespace DocumentUpoadAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Document context dependecy injection with db connection string
+            services.AddDbContext<DocumentContext>(o => o.UseSqlServer("Data source = document.db"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
