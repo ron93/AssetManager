90d88a9ac67069ac04dff3a1a34cb19ae79a0b5c
ron93 <ronmkare1@gmail.com>
2021-07-13 17:52:02 +0300

    added data annotation

--- Models/Documents.cs
+++ Models/Documents.cs
@@ -1,15 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DocumentUpoadAPI
+namespace DocumentUpoadAPI.Models
 {
     public class Document
     {
         public int Id;
+        [Required]
+        [StringLength(30, MinimumLength =3)]
+        public string Name;
+
+        [Required]
+        [StringLength(100, MinimumLength =5)]
         public string Description;
+
+        [DataType(DataType.Date)]
+        public DateTime UploadDate { get; set; }
         public string Path;
-        
+
     }
 }
