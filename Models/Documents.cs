420b026b9396469805f2992b1a5d0f4fc5329138
ron93 <ronmkare1@gmail.com>
2021-07-13 16:53:12 +0300

    Documents Model

---
+++ Models/Documents.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentUpoadAPI
+{
+    public class Document
+    {
+        public int Id;
+        public string Description;
+        public string Path;
+        
+    }
+}
