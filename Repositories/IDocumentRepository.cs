7c30ab17a19a6a57f813653d620210d2a364d904
ron93 <ronmkare1@gmail.com>
2021-07-14 09:20:41 +0300

    comments added

--- Repositories/IDocumentRepository.cs
+++ Repositories/IDocumentRepository.cs
@@ -7,15 +7,19 @@ namespace DocumentUpoadAPI.Repositories
 {
     public interface IDocumentRepository 
     {
-
+        //task to get all documents
         Task<IEnumerable<Document>> GetDocuments();
 
+        //task to get a document
         Task<Document> GetDocument(int id);
 
+        //task to create a document
         Task<Document> Create(Document document);
 
+        //task to update a document
         Task Update(Document document);
 
+        //task to delete a document
         Task Delete(int id);
     }
 }
